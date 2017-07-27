7e51c020989dfc3cd1a889adf7b65ca42b0b3f18
Larry Hosken <larry.hosken@token.io>
2017-07-27 13:31:45 -0700

    better

---
+++ README.md
@@ -0,0 +1,21 @@
+## Token Merchant Checkout Sample: Java
+
+**To create a member:**
+
+* Edit `setup/src/main/java/io/token/setup/Application.java` and replace the
+  username `marianoTest5` with another alphanumeric string.
+* Compile: `./gradlew shadowJar`
+* Run the setup utility: `java -jar setup/build/libs/setup-1.0.1-all.jar`
+* Notice the username output at the end of the output.
+
+This creates a new member on the Token network and stores its private keys
+in the `./keys` directory.
+
+**To run the server:**
+
+1. In `app/src/main/resources/index.html`, change the username to the one used above.
+2. In `app/src/main/java/io/token/sample/Application.java`, in the `tokenIO.login`
+   call, change the memberId to the output from the setup program.
+4. Compile and run: `./gradlew shadowJar` ;
+   `java -jar setup/build/libs/setup-1.0.1-all.jar`
+5. Test by going to localhost:3000, and paying with the "Token PSD2" app, installed from the App Store
--- app/src/main/java/io/token/sample/Application.java
+++ app/src/main/java/io/token/sample/Application.java
@@ -1,9 +1,16 @@
 package io.token.sample;
 
+import static com.google.common.base.Charsets.UTF_8;
 import static io.token.TokenIO.TokenCluster.SANDBOX;
 
+import com.google.gson.JsonParser;
+import io.token.proto.common.token.TokenProtos.Token;
+import com.google.common.io.Resources;
+import io.token.Member;
 import io.token.TokenIO;
 import io.token.security.UnsecuredFileSystemKeyStore;
+
+import spark.Spark;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,9 +18,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * Application main entry point. 
+ * Application main entry point.
  * To execute, one needs to run something like:
- *
+ * <p>
  * <pre>
  * ./gradlew :app:shadowJar
  * java -jar ./app/build/libs/app-1.0.0-all.jar
@@ -27,10 +34,32 @@ public class Application {
      * @throws IOException thrown on errors
      */
     public static void main(String[] args) throws IOException {
-        // Now all the command line parameters have been parsed. So we can
-        // initialize the SDK.
+        // Connect to Token's development sandbox
         TokenIO tokenIO = initializeSDK();
-        System.out.printf("tokenIo = %s \n", tokenIO.toString());
+
+        // Log in using the test merchant account created
+        // by the setup utility:
+        Member loggedInMember = tokenIO.login("m:28i4rgDUaM7sUBMCaP4ZfHJpnmMX:5zKtXEAq");
+
+        // Initializes the server
+        Spark.port(3000);
+
+        // Endpoint for transfering, called by client side after user approval
+        Spark.post("/transfer", (req, res) -> {
+            String tokenId = new JsonParser()
+                    .parse(req.body())
+                    .getAsJsonObject()
+                    .get("tokenId")
+                    .getAsString();
+            
+            // Make sure to get the token first, and check its validity
+            Token token = loggedInMember.getToken(tokenId);
+            // Redeem the token at the server, to move the funds
+            loggedInMember.redeemToken(token, 4.99, "EUR", "example");
+            return "";
+        });
+        String page = Resources.toString(Resources.getResource("index.html"), UTF_8);
+        Spark.get("/", (req, res) -> page);
     }
 
     /**
@@ -41,8 +70,7 @@ public class Application {
      * @throws IOException
      */
     private static TokenIO initializeSDK() throws IOException {
-
-        Path keys = Files.createDirectories(Paths.get("./"));
+        Path keys = Files.createDirectories(Paths.get("./keys"));
         return TokenIO.builder()
                 .connectTo(SANDBOX)
                 .withKeyStore(new UnsecuredFileSystemKeyStore(keys.toFile()))
