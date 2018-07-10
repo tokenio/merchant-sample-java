9cb366ed595e117c02a095a1ca2f98484aa544a3
Greco Rubio <greco.rubio@token.io>
2018-07-10 18:58:14 -0500

    New button API and type selector

--- app/src/main/resources/script.js
+++ app/src/main/resources/script.js
@@ -1,20 +1,97 @@
 "use strict";
 
 var elementId = "tokenPayBtn";
+var tokenController;
+var button;
 
-function createButton() {
-    // Create button
-    window.Token.styleButton({
-        id: elementId,
-        label: "Token Quick Checkout",
-    }, bindButton); // execute bindButton when styling button is finished
+function clean() {
+    if (button) {
+        button.destroy();
+        button = null;
+    }
+
+    if (tokenController && tokenController.destroy) {
+        tokenController.destroy();
+        tokenController = null;
+    }
 }
 
-function bindButton(button) {
+function createRedirectButton() {
+    // clean up instances
+    clean();
+
+    // create TokenPopupController to handle Popup messages
+    tokenController = window.Token.createRedirectController();
+
+    // get button placeholder element
+    var element = document.getElementById(elementId);
+
+    // create the button
+    button = window.Token.createTokenButton(element, {
+        label: "Redirect Token Quick Checkout",
+    });
+
+    // bind the Token Button to the Redirect Controller when ready
+    tokenController.bindButtonClick(button, function(action) {
+    // Each time the button is clicked, a new tokenRequestUrl is created
+        getTokenRequestUrl(function(tokenRequestUrl) {
+            // Initialize popup using the tokenRequestUrl
+            action(tokenRequestUrl);
+        });
+    });
+    // enable button after binding
+    button.enable();
+}
+
+function createPopupButton() {
+    // clean up instances
+    clean();
+
+    // create TokenPopupController to handle Popup messages
+    tokenController = window.Token.createPopupController();
+
+    // get button placeholder element
+    var element = document.getElementById(elementId);
+
+    // create the button
+    button = window.Token.createTokenButton(element, {
+        label: "Popup Token Quick Checkout",
+    });
+
+    // setup onLoad callback
+    tokenController.onLoad(function(controller) {
+        // bind the Token Button to the Popup Controller when ready
+        tokenController.bindButtonClick(button, function(action) {
+            // Each time the button is clicked, a new tokenRequestUrl is created
+            getTokenRequestUrl(function(tokenRequestUrl) {
+                // Initialize popup using the tokenRequestUrl
+                action(tokenRequestUrl);
+            });
+        });
+        // enable button after binding
+        button.enable();
+    });
+
+    // setup onSuccess callback
+    tokenController.onSuccess(function(data) { // Success Callback
+        // build success URL
+        var successURL = "/redeem"
+            + "?tokenId=" + window.encodeURIComponent(data.tokenId);
+        // navigate to success URL
+        window.location.assign(successURL);
+    });
+
+    // setup onError callback
+    tokenController.onError(function(error) { // Failure Callback
+        throw error;
+    });
+}
+
+function getTokenRequestUrl(done) {
     var XHR = new XMLHttpRequest();
 
-    // Set up our request
-    XHR.open('POST', 'http://localhost:3000/transfer', true);
+    //set up the access request
+    XHR.open("POST", "http://localhost:3000/transfer", true);
 
     XHR.setRequestHeader("Content-Type", "application/json; charset=utf-8");
 
@@ -28,24 +105,35 @@ function bindButton(button) {
 
      // Define what happens on successful data submission
      XHR.addEventListener("load", function(event) {
-       button.bindPayButton(
-            event.target.responseURL, // request token URL
-            null, // shipping callback (deprecated)
-            function(data) {
-                // build success URL
-                var successURL = "/redeem"
-                    + "?tokenId=" + window.encodeURIComponent(data.tokenId);
-                // navigate to success URL
-                window.location.assign(successURL);
-            },
-            function(error) { // fail callback
-                throw error;
-            }
-       );
+         // execute callback once response is received
+         if (event.target.status === 200) {
+             done(event.target.responseURL);
+         }
      });
 
     // Send the data; HTTP headers are set automatically
     XHR.send(data);
 }
 
-createButton();
+function setupButtonTypeSelector() {
+    var selector = document.getElementsByName('buttonTypeSelector');
+    var selected;
+    for (var i = 0; i < selector.length; i++) {
+        if (selector[i].checked) {
+            selected = selector[i].value;
+        }
+        selector[i].addEventListener('click', function(e) {
+            var value = e.target.value;
+            if (value === selected) return;
+            if (value === 'popup') {
+                createPopupButton();
+            } else if (value === 'redirect') {
+                createRedirectButton();
+            }
+            selected = value;
+        });
+    }
+    createPopupButton();
+}
+
+setupButtonTypeSelector();
