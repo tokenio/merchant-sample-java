43bfb9cbc917be7d272af4843b9038b17fb09124
Greco Rubio <grekinsky@users.noreply.github.com>
2018-07-10 19:45:07 -0500

    New button api (#29)
    * popup functionality for sample code
    
    * New button API and type selector

--- app/src/main/resources/script.js
+++ app/src/main/resources/script.js
@@ -1,11 +1,97 @@
-function initiatePayment() {
-    // prevent multiple clicking
-    unbindClick();
+"use strict";
 
+var elementId = "tokenPayBtn";
+var tokenController;
+var button;
+
+function clean() {
+    if (button) {
+        button.destroy();
+        button = null;
+    }
+
+    if (tokenController && tokenController.destroy) {
+        tokenController.destroy();
+        tokenController = null;
+    }
+}
+
+function createRedirectButton() {
+    // clean up instances
+    clean();
+
+    // create TokenPopupController to handle Popup messages
+    tokenController = window.Token.createRedirectController();
+
+    // get button placeholder element
+    var element = document.getElementById(elementId);
+
+    // create the button
+    button = window.Token.createTokenButton(element, {
+        label: "Redirect Token Quick Checkout",
+    });
+
+    // bind the Token Button to the Redirect Controller when ready
+    tokenController.bindButtonClick(button, function(action) {
+    // Each time the button is clicked, a new tokenRequestUrl is created
+        getTokenRequestUrl(function(tokenRequestUrl) {
+            // Initialize popup using the tokenRequestUrl
+            action(tokenRequestUrl);
+        });
+    });
+    // enable button after binding
+    button.enable();
+}
+
+function createPopupButton() {
+    // clean up instances
+    clean();
+
+    // create TokenPopupController to handle Popup messages
+    tokenController = window.Token.createPopupController();
+
+    // get button placeholder element
+    var element = document.getElementById(elementId);
+
+    // create the button
+    button = window.Token.createTokenButton(element, {
+        label: "Popup Token Quick Checkout",
+    });
+
+    // setup onLoad callback
+    tokenController.onLoad(function(controller) {
+        // bind the Token Button to the Popup Controller when ready
+        tokenController.bindButtonClick(button, function(action) {
+            // Each time the button is clicked, a new tokenRequestUrl is created
+            getTokenRequestUrl(function(tokenRequestUrl) {
+                // Initialize popup using the tokenRequestUrl
+                action(tokenRequestUrl);
+            });
+        });
+        // enable button after binding
+        button.enable();
+    });
+
+    // setup onSuccess callback
+    tokenController.onSuccess(function(data) { // Success Callback
+        // build success URL
+        var successURL = "/redeem"
+            + "?tokenId=" + window.encodeURIComponent(data.tokenId);
+        // navigate to success URL
+        window.location.assign(successURL);
+    });
+
+    // setup onError callback
+    tokenController.onError(function(error) { // Failure Callback
+        throw error;
+    });
+}
+
+function getTokenRequestUrl(done) {
     var XHR = new XMLHttpRequest();
 
-    // Set up our request
-    XHR.open('POST', 'http://localhost:3000/transfer', true);
+    //set up the access request
+    XHR.open("POST", "http://localhost:3000/transfer", true);
 
     XHR.setRequestHeader("Content-Type", "application/json; charset=utf-8");
 
@@ -19,22 +105,35 @@ function initiatePayment() {
 
      // Define what happens on successful data submission
      XHR.addEventListener("load", function(event) {
-       window.location.assign(event.target.responseURL);
+         // execute callback once response is received
+         if (event.target.status === 200) {
+             done(event.target.responseURL);
+         }
      });
 
     // Send the data; HTTP headers are set automatically
     XHR.send(data);
 }
 
-function bindClick() {
-    // Add click listener
-    el.addEventListener('click', initiatePayment);
+function setupButtonTypeSelector() {
+    var selector = document.getElementsByName('buttonTypeSelector');
+    var selected;
+    for (var i = 0; i < selector.length; i++) {
+        if (selector[i].checked) {
+            selected = selector[i].value;
+        }
+        selector[i].addEventListener('click', function(e) {
+            var value = e.target.value;
+            if (value === selected) return;
+            if (value === 'popup') {
+                createPopupButton();
+            } else if (value === 'redirect') {
+                createRedirectButton();
+            }
+            selected = value;
+        });
+    }
+    createPopupButton();
 }
 
-function unbindClick() {
-    // Remove click listener
-    el.removeEventListener('click', initiatePayment);
-}
-
-var el = document.getElementById("tokenPayBtn");
-bindClick();
+setupButtonTypeSelector();
