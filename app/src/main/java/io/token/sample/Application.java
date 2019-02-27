69a59b08a934de6f2263673bc9119423ff046994
Gianluca Pane <31416110+gianluca-token@users.noreply.github.com>
2019-02-27 13:32:10 -0800

    RD-2057: use parseTokenRequestCallbackParams (#35)

--- app/src/main/java/io/token/sample/Application.java
+++ app/src/main/java/io/token/sample/Application.java
@@ -18,6 +18,7 @@ import io.token.security.UnsecuredFileSystemKeyStore;
 import io.token.tokenrequest.TokenRequest;
 import io.token.tpp.Member;
 import io.token.tpp.TokenClient;
+import io.token.tpp.tokenrequest.TokenRequestCallback;
 
 import java.io.File;
 import java.io.IOException;
@@ -29,6 +30,7 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import spark.Spark;
 
@@ -42,6 +44,8 @@ import spark.Spark;
  * </pre>
  */
 public class Application {
+    private static final String CSRF_TOKEN_KEY = "csrf_token";
+
     /**
      * Main function.
      *
@@ -70,6 +74,12 @@ public class Application {
                     formData.get("destination"),
                     BankAccount.newBuilder());
 
+            // generate CSRF token
+            String csrfToken = generateNonce();
+
+            // set CSRF token in browser cookie
+            res.cookie(CSRF_TOKEN_KEY, csrfToken);
+
             // create the token request
             TokenRequest request = TokenRequest.transferTokenRequestBuilder(amount, currency)
                     .setDescription(formData.get("description"))
@@ -79,7 +89,7 @@ public class Application {
                     .setToAlias(merchantMember.firstAliasBlocking())
                     .setToMemberId(merchantMember.memberId())
                     .setRedirectUrl("http://localhost:3000/redeem")
-                    .setCallbackState(generateNonce())
+                    .setCsrfToken(csrfToken)
                     .build();
 
             String requestId = merchantMember.storeTokenRequestBlocking(request);
@@ -93,9 +103,20 @@ public class Application {
         });
 
         Spark.get("/redeem", (req, res) -> {
-            String tokenId = req.queryMap("tokenId").value();
+            Map<String, String> queryParams = req.queryParams()
+                    .stream()
+                    .collect(Collectors.toMap(k -> k, k -> req.queryParams(k)));
+
+            // retrieve CSRF token from browser cookie
+            String csrfToken = req.cookie(CSRF_TOKEN_KEY);
+
+            // check CSRF token and retrieve state and token ID from callback parameters
+            TokenRequestCallback callback = tokenClient.parseTokenRequestCallbackParamsBlocking(
+                    queryParams,
+                    csrfToken);
+
             //get the token and check its validity
-            Token token = merchantMember.getTokenBlocking(tokenId);
+            Token token = merchantMember.getTokenBlocking(callback.getTokenId());
 
             //redeem the token at the server to move the funds
             Transfer transfer = merchantMember.redeemTokenBlocking(token);
